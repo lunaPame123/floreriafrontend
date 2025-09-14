df792da58b3456de3e0b58cdc7b5bac706846f13
PamelaCanaza <lunapamela886@gmail.com>
2025-09-14 02:44:53 -0400

    feat: crear estructura básica del componente Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input type="password" />
+        </div>
+        <button>Ingresar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
