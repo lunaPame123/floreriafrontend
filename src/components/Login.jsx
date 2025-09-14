7d8c6ed55d39bec659ac6aa103259da2e684b302
PamelaCanaza <lunapamela886@gmail.com>
2025-09-14 02:55:41 -0400

    feat: agregar estados e inputs controlados en Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+const Login = ({ onLogin }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
   return (
-    <div>
+    <div style={{ maxWidth: "400px", margin: "auto", paddingTop: "50px" }}>
       <h2>Iniciar Sesión</h2>
       <form>
         <div>
           <label>Email:</label>
-          <input type="email" />
+          <input 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required 
+          />
         </div>
         <div>
           <label>Contraseña:</label>
-          <input type="password" />
+          <input 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+         />
         </div>
-        <button>Ingresar</button>
+        <button type="submit">Ingresar</button>
       </form>
     </div>
   );
