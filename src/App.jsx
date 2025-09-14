b390ed67700e4f977c2e87c151143f5991590c23
PamelaCanaza <lunapamela886@gmail.com>
2025-09-14 03:10:56 -0400

    feat: integrar Login y BandejaUsuarios en App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Login from "./components/Login";
+import BandejaUsuarios from "./components/BandejaUsuarios";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {!user ? (
+        <Login onLogin={setUser} />
+      ) : (
+        <BandejaUsuarios />
+      )}
+    </div>
+  )
+}
+
+export default App;
